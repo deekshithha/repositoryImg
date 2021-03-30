a0a1b051e6c0db0efb7cc206105e198f9ff0237c
unknown <deekshiavru@gmail.com>
2021-03-30 11:53:16 +0530

    ImageContainerRepo

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,73 @@
+﻿using BuildPipeEditDockerProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BuildPipeEditDockerProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly IRepository<Movie> _repository;
+        public MovieController(IRepository<Movie> repository)
+        {
+            _repository = repository;
+        }
+        // GET: api/<MovieController>
+        [HttpGet]
+        public ActionResult<IEnumerable<Movie>> Get()
+        {
+            var movie = _repository.GetItems();
+            if (movie.Count() == 0)
+                return BadRequest("No movie added till now");
+            else
+                return Ok(movie);
+        }
+
+        // GET api/<MovieController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Movie> Get(int id)
+        {
+            var movie = _repository.GetItemId(id);
+            if (movie == null)
+            {
+                return BadRequest("No movie in given id");
+            }
+            else
+                return Ok(movie);
+            
+        }
+
+        // POST api/<MovieController>
+        [HttpPost]
+        public ActionResult<Movie> Post([FromBody] Movie movie)
+        {
+            try
+            {
+                _repository.AddItem(movie);
+                return Ok(movie);
+            }
+            catch
+            {
+                return BadRequest("Cannot add the item");
+            }
+        }
+
+        // PUT api/<MovieController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MovieController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/MoviesRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuildPipeEditDockerProject.Models
+{
+    public class MoviesRepository:IRepository<Movie>
+    {
+        public List<Movie> movies;
+
+        public MoviesRepository()
+        {
+            movies = new List<Movie>();
+            movies.Add(new Movie
+            {
+                Id = 101,
+                Name = "x-Men",
+                Duration=120.0f
+            });
+        }
+
+        public void AddItem(Movie movie)
+        {
+            this.movies.Add(movie);
+        }
+
+        public Movie GetItemId(int id)
+        {
+            return this.movies.FirstOrDefault(m => m.Id == id);
+        }
+
+        public IEnumerable<Movie> GetItems()
+        {
+            return this.movies;
+        }
+    }
+}
